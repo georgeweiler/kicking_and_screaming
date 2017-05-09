f5dfbf51c70c8929839509fe72cc1b63c3994c86
George <georgejweiler@gmail.com>
2017-05-08 20:29:55 -0700

    fix file structure for heroku:X

--- public/index.bundle.js
+++ public/index.bundle.js
@@ -1,130 +1,18 @@
 "use strict";
 
-var globalPlayers = [{
-  "name": "1",
-  "position": "second base",
-  "isStarter": true,
-  "hasSub": true,
-  "sub": "15"
-}, {
-  "name": "2",
-  "position": "left outfield",
-  "isStarter": true,
-  "hasSub": true,
-  "sub": "20"
-}, {
-  "name": "3",
-  "position": "center outfield",
-  "isStarter": true,
-  "hasSub": true,
-  "sub": "12"
-}, {
-  "name": "4",
-  "position": "right outfield",
-  "isStarter": true,
-  "hasSub": false
-}, {
-  "name": "5",
-  "position": "thurd base",
-  "isStarter": true,
-  "hasSub": true,
-  "sub": "14"
-}, {
-  "name": "6",
-  "position": "pitcher",
-  "isStarter": true,
-  "hasSub": true,
-  "sub": "18"
-}, {
-  "name": "7",
-  "position": "right center",
-  "isStarter": true,
-  "hasSub": true,
-  "sub": "17"
-}, {
-  "name": "8",
-  "position": "first base",
-  "isStarter": true,
-  "hasSub": true,
-  "sub": "13"
-}, {
-  "name": "9",
-  "position": "short stop",
-  "isStarter": true,
-  "hasSub": true,
-  "sub": "16"
-}, {
-  "name": "10",
-  "position": "catcher",
-  "isStarter": true,
-  "hasSub": true,
-  "sub": "19"
-}, {
-  "name": "11",
-  "position": "left center",
-  "isStarter": false,
-  "hasSub": false
-}, {
-  "name": "12",
-  "position": "Sub for: 3",
-  "hasSub": true,
-  "sub": "3",
-  "isStarter": false
-}, {
-  "name": "13",
-  "position": "Sub for: 8",
-  "hasSub": true,
-  "sub": "8",
-  "isStarter": false
-}, {
-  "name": "14",
-  "position": "Sub for: 5",
-  "hasSub": true,
-  "sub": "5",
-  "isStarter": false
-}, {
-  "name": "15",
-  "position": "Sub for: 1",
-  "hasSub": true,
-  "sub": "1",
-  "isStarter": false
-}, {
-  "name": "16",
-  "position": "Sub for: 9",
-  "hasSub": true,
-  "sub": "9",
-  "isStarter": false
-}, {
-  "name": "17",
-  "position": "Sub for: 7",
-  "hasSub": true,
-  "sub": "7",
-  "isStarter": false
-}, {
-  "name": "18",
-  "position": "Sub for: 6",
-  "hasSub": true,
-  "sub": "6",
-  "isStarter": false
-}, {
-  "name": "19",
-  "position": "Sub for: 10",
-  "hasSub": true,
-  "sub": "10",
-  "isStarter": false
-}, {
-  "name": "20",
-  "position": "Sub for: 2",
-  "hasSub": true,
-  "sub": "2",
-  "isStarter": false
-}];
+var globalPlayers = [];
 
 var globalPositions = ["pitcher", "catcher", "first base", "second base", "thurd base", "short stop", "left center", "right center", "left outfield", "center outfield", "right outfield"];
 var globalBattingLineup = [];
 var globalCurrentlyAtBat = void 0;
 var globalPlayersWithoutSubs = void 0;
+var globalKScore = 0;
+var globalOtherScore = 0;
+
 function addPlayer() {
+  if ($('#add-player-input').val() === "") {
+    return;
+  }
   var playerObj = {
     name: $('#add-player-input').val()
   };
@@ -137,7 +25,7 @@ function addPlayer() {
 
     // 11 people already assigned -- START ASSIGNING SUBS
     if (globalPositions.length === 0) {
-      //LMAO GOOD LUCK UNDERSTANDING THIS SHIT
+      //LOL GOOD LUCK UNDERSTANDING THIS SHIT
       globalPlayersWithoutSubs = _.filter(globalPlayers, function (player) {
         return !player.hasSub;
       }).map(function (player) {
@@ -171,6 +59,10 @@ function addPlayer() {
 }
 
 function createBattingLineup() {
+  if (globalPlayers.length < 11) {
+    alert('you need more players');
+    return;
+  }
   var shuffledPlayers = _.shuffle(globalPlayers);
   var alreadyAdded = [];
   while (shuffledPlayers.length) {
@@ -178,9 +70,11 @@ function createBattingLineup() {
     if (!randomPlayer.hasSub) {
       globalBattingLineup.push([randomPlayer.name]);
     } else {
-      if (!_.contains(alreadyAdded, randomPlayer.name) || !_.contains(alreadyAdded, randomPlayer.sub)) alreadyAdded.push(randomPlayer.name);
-      alreadyAdded.push(randomPlayer.sub);
-      globalBattingLineup.push([randomPlayer.name, randomPlayer.sub]);
+      if (!_.contains(alreadyAdded, randomPlayer.name) || !_.contains(alreadyAdded, randomPlayer.sub)) {
+        alreadyAdded.push(randomPlayer.name);
+        alreadyAdded.push(randomPlayer.sub);
+        globalBattingLineup.push([randomPlayer.name, randomPlayer.sub]);
+      }
     }
   }
   rotateBattingLineup();
@@ -206,11 +100,19 @@ function rotateBattingLineup() {
 function nextAtBat() {
   rotateBattingLineup();
 }
-
-// function removePlayer(btn){
-//   let player = $(btn).parent().siblings('.player-td').text()
-//   let position = $(btn).parent().siblings('.position-td').text()
-//   globalPlayers = _.without(globalPlayers, _.findWhere(globalPlayers, {name: player}));
-//   globalPositions.push(position)
-//   $(btn).closest('tr').remove();
-// }
+function addKScore() {
+  globalKScore++;
+  $("#ks-score").text(globalKScore);
+}
+function addOScore() {
+  globalOtherScore++;
+  $("#other-score").text(globalOtherScore);
+}
+function minusKScore() {
+  globalKScore--;
+  $("#ks-score").text(globalKScore);
+}
+function minusOScore() {
+  globalOtherScore--;
+  $("#other-score").text(globalOtherScore);
+}
